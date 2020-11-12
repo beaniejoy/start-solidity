be4028fde5fbbb4d1b3e23a10875dd765700a017
Hanbin Lee <lhb1025@gmail.com>
2020-11-13 01:05:25 +0900

    Update Vote.sol

--- Vote.sol
+++ Vote.sol
@@ -1,1 +1,41 @@
+pragma solidity 8.4.23;
 
+contract Vote {
+    
+    struct candidator {
+        string name;
+        uint upVote;
+    }
+    
+    // variable
+    candidator[] public candidatorList;
+    
+    // mapping
+    
+    // event
+    event AddCandidator(string name);
+    
+    // modifier
+    
+    // constructor
+    
+    // candidator
+    function addCandidator(string _name) public {
+        require(candidatorList.length < 5)
+        candidatorList.push(candidator(_name, 0));
+        
+        // emit event
+        emit AddCandidator(_name);
+        
+    }
+    
+    // voting
+    function Voting() public {
+        
+    }
+    
+    // finish vote
+    function finishVote() public {
+        
+    }
+}
