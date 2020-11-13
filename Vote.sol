27ac12aef5362f2c2e152dcb3cde2ee2c383998e
Hanbin Lee <lhb1025@gmail.com>
2020-11-14 00:36:27 +0900

    201114 Vote System upload

--- Vote.sol
+++ Vote.sol
@@ -1,4 +1,6 @@
-pragma solidity 8.4.23;
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.7;
 
 contract Vote {
     
@@ -8,34 +10,65 @@ contract Vote {
     }
     
     // variable
-    candidator[] public candidatorList;
+    bool live; // for checking whether the Vote is valid or not 
+    address owner; // who makes the Vote.
+    candidator[] public candidatorList; // enrolled canditators list in the Vote
     
     // mapping
+    // whether voting is done or not
+    mapping(address => bool) Voted; // key: address, value: bool
     
     // event
     event AddCandidator(string name);
+    event UpVote(string candidator, uint upVote);
+    event FinishVote(bool live);
+    event Voting(address owner);
     
     // modifier
+    // for controlling finishVote.
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
     
     // constructor
+    constructor() public {
+        owner = msg.sender;
+        live = true; // not yet voting
+        
+        emit Voting(owner);
+    }
     
-    // candidator
-    function addCandidator(string _name) public {
-        require(candidatorList.length < 5)
+    // enroll candidator
+    function addCandidator(string memory _name) public onlyOwner{
+        require(live == true);
+        require(candidatorList.length < 5); // because of gas cost, limit 5 candidators
         candidatorList.push(candidator(_name, 0));
         
-        // emit event
+        // announce the event
         emit AddCandidator(_name);
         
     }
     
     // voting
-    function Voting() public {
+    function upVote(uint _indexOfCandidator) public {
+        require(live == true);
+        require(_indexOfCandidator < candidatorList.length);
+        require(Voted[msg.sender] == false);
+        candidatorList[_indexOfCandidator].upVote++;
         
+        // check voting done
+        Voted[msg.sender] = true;
+        
+        // announce voting infomation
+        emit UpVote(candidatorList[_indexOfCandidator].name, candidatorList[_indexOfCandidator].upVote);
     }
     
     // finish vote
-    function finishVote() public {
+    function finishVote() public onlyOwner {
+        live = false;
         
+        // announce finishing Vote totally
+        emit FinishVote(live);
     }
 }
